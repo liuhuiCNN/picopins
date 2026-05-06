80bc018ee163eeb4c03896ac828f9e77a55cd2b3
cnn <hui.liu.cnn@gmail.com>
2026-05-06 10:51:43 +0800

    update PINOUT.
    update PINOUT, set version=1.3.0

--- picopins/__main__.py
+++ picopins/__main__.py
@@ -18,13 +18,13 @@ Shout-out to Raspberry Pi Spy for having almost this exact idea first:
 https://www.raspberrypi-spy.co.uk/2022/12/pi-pico-pinout-display-on-the-command-line/
 """
 
-__version__ = '1.1.0'
+__version__ = '1.3.0'
 
 PINOUT = [line.split("|") for line in """
       |         |        |        |      |  |     ┏━━━━━┓     |  |          |        |        |         |
       |         |        |        |      |  |┏━━━━┫     ┣━━━━┓|  |          |        |        |         |
 PWM0 A|UART0 TX |I2C0 SDA|SPI0 RX |GP0   |1 |┃◎   ┗━━━━━┛   ◎┃|40|VBUS      |        |        |         |
-PWM0 B|UART0 RX |I2C0 SCL|SPI0 CSn|GP1   |2 |┃◎ ▩          ◎┃|39|VSYS      |        |        |         |
+PWM0 B|UART0 RX |I2C0 SCL|SPI0 CSn|GP1   |2 |┃◎ ▩           ◎┃|39|VSYS      |        |        |         |
       |         |        |        |Ground|3 |┃▣ └─GP25      ▣┃|38|Ground    |        |        |         |
 PWM1 A|UART0 CTS|I2C1 SDA|SPI0 SCK|GP2   |4 |┃◎  ▒▒▒        ◎┃|37|3v3 En    |        |        |         |
 PWM1 B|UART0 RTS|I2C1 SCL|SPI0 TX |GP3   |5 |┃◎  ▒▒▒        ◎┃|36|3v3 Out   |        |        |         |
@@ -42,7 +42,7 @@ PWM6 A|UART0 TX |I2C0 SDA|SPI1 RX |GP12  |16|┃◎             ◎┃|25|GP19  
 PWM6 B|UART0 RX |I2C0 SCL|SPI1 CSn|GP13  |17|┃◎             ◎┃|24|GP18      |SPI0 SCK|I2C1 SDA|UART0 CTS|PWM1 A
       |         |        |        |Ground|18|┃▣             ▣┃|23|Ground    |        |        |         |
 PWM7 A|UART0 CTS|I2C1 SDA|SPI1 SCK|GP14  |19|┃◎             ◎┃|22|GP17      |SPI0 CSn|I2C0 SCL|UART0 RX |PWM0 B
-PWM7 B|UART0 RTS|I2C1 SCL|SPI1 TX |GP15  |20|┃◎  ◎ ▣ ◎   ◎┃|21|GP16      |SPI0 RX |I2C0 SDA|UART0 TX |PWM0 A
+PWM7 B|UART0 RTS|I2C1 SCL|SPI1 TX |GP15  |20|┃◎    ◎ ▣ ◎    ◎┃|21|GP16      |SPI0 RX |I2C0 SDA|UART0 TX |PWM0 A
       |         |        |        |      |  |┗━━━━━━━━━━━━━━━┛|  |          |        |        |         |
 """.splitlines()[1:]]
 
